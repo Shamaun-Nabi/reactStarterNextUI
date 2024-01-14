6a13fd47fe283f882098902cce8ba9f2c0a14ce8
Shamaun-Nabi@ <dev.shamaun@gmail.com>
2024-01-14 14:52:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@nextui-org/react";
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <Button color="primary">Welcome NEXT UI</Button>
+    </div>
+  );
+}
